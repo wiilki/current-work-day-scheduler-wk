b47b0ac564181d9c3402215264aa11c16f6d513d
Will <willkim0608@gmail.com>
2022-11-12 22:29:34 -0800

    Add .each function for textareas

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,25 +1,6 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
+
 $(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
+
 
 
   var today = dayjs();
@@ -51,24 +32,79 @@ $(function () {
   $('section div').each(function () {
     const d = new Date();
     d.setHours(startHour, 0, 0);
-  
+    this.append(d);
+    startHour++;
+  });
+
+
+  var taskFormEl = $('.task-form');
+  var taskArray = [];
+
+  $('section div').each(function () {
+    const d = new Date();
+    d.setHours(startHour, 0, 0);
     this.append(d);
     startHour++;
   });
 
 
 
+  // Listener for the save button
+  function handleFormSubmit(event) {
+    event.preventDefault();
+
+    //Goes through each textarea element. Adds to array, then resets the value of the area
+    $('textarea').each(function () {
+      // Take textarea input and push to array
+      var descriptionInput = $('textarea').val();
+      taskArray.push(descriptionInput);
+      // Clears current textarea before going to next textarea
+      $('textarea').val('')
+    });
+
+  }
+
+  // Create a submit event listener on the form element
+  taskFormEl.on('submit', handleFormSubmit);
+
+  console.log(taskArray)
+
+  // ON CLICK
+
+  // READ INNERHTML OF J=0 TEXTAREA
+  // ADD TO Array
+  // READ INNERHTML OF J=1 TEXTAREA
+  // ADD TO ARRAY
 
 
 
-  // var startHour = dayjs().set('hour', 9).format('hA');
-  // var displayHour = startHour;
-
-  // const d = new Date();
-  // d.setHours(9, 0, 0);
- 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  // Listener for the save button
+  // function handleFormSubmit(event) {
+
+
+  //   // Stringify array and save to local storage
+  //   localStorage.setItem("current-tasks", JSON.stringify(taskArray))
+  // }
 
 
   // var taskFormEl = $('.task-form');
@@ -82,16 +118,17 @@ $(function () {
   //   var taskInfo = {
   //     taskDescription: descriptionInput,
   //   }
+
   //   // Push object to array
   //   taskArray.push(taskInfo);
+
   //   // Stringify array and save to local storage
   //   localStorage.setItem("current-tasks", JSON.stringify(taskArray))
   // }
 
 
 
-
-  // // Create a submit event listener on the form element
+  // Create a submit event listener on the form element
   // taskFormEl.on('submit', handleFormSubmit);
 
   // function init() {
@@ -104,7 +141,7 @@ $(function () {
   //   for (var i = 0; i < taskArray.length; i++) {
 
   //     // Test code
-  //     console.log(taskArray[i]);
+  //     $('textarea').append(taskArray[i]);
   //   }
   // }
 
