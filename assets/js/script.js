75425a9dd46d3e13a9d89427ee547afd802c379b
Will <willkim0608@gmail.com>
2022-11-13 22:37:15 -0800

    Add clearArray function to fix oversized array issue. Wrapped codes into multiple functions then call functions at bottom to keep code cleaner. Add preventdefault method.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,15 @@
-
 $(function () {
   var template = document.getElementById("hour-row-template").innerHTML;
   var container = $('#container');
   var taskArray = [];
   var startHour = 9;
   var dayRange = 9;
+  // Today's Date
+  var today = dayjs();
   // Get stored taskArray from localStorage
   var storedTasks = JSON.parse(localStorage.getItem("current-tasks"));
 
-  // Today's Date
-  var today = dayjs();
+
   // Add today's date to header
   $('#currentDay').text(today.format('dddd, MMMM D'));
 
@@ -24,75 +24,110 @@ $(function () {
   }
 
   // Adds template content to container 9 times
-  for (i = 0; i < dayRange; i++) {
-    tempToCont();
-  };
+  function addTemplate() {
+    for (i = 0; i < dayRange; i++) {
+      tempToCont();
+    };
+  }
 
   // Adds each value of storedTasks back to corresponding textarea
-  for (j = 0; j < dayRange; j++) {
-    var containerEl = document.getElementById("container");
-    if (storedTasks) {
-      containerEl.children[j + 1].children[0].children[1].append(storedTasks[j])
-    }
-
-  };
+  function renderArray() {
+    for (j = 0; j < dayRange; j++) {
+      var containerEl = document.getElementById("container");
+      if (storedTasks) {
+        containerEl.children[j + 1].children[0].children[1].append(storedTasks[j])
+      }
+    };
+  }
 
   // Set id to each section inside container and increments id#
-  resetHour();
-  $('form').each(function () {
-    const hourEl = new Date();
-    hourEl.setHours(startHour);
-    this.setAttribute("id", "hour-" + startHour);
-    startHour++;
-  });
+  function setIds() {
+    resetHour();
+    $('form').each(function () {
+      const hourEl = new Date();
+      hourEl.setHours(startHour);
+      this.setAttribute("id", "hour-" + startHour);
+      startHour++;
+    });
+  }
 
   // Set startHour back to 9. Append hour displays to rows
-  resetHour();
-  $('section div').each(function () {
-    const hourEl = new Date();
-    hourEl.setHours(startHour);
-
-    // Change time display format
-    if (hourEl.getHours() < 12) {
-      this.append(hourEl.getHours() + "AM");
-    } else if (hourEl.getHours() > 12) {
-      this.append(hourEl.getHours() - 12 + "PM");
-    } else {
-      this.append(hourEl.getHours() + "PM");
-    }
-    startHour++;
-  });
+  function displayHours() {
+    resetHour();
+    $('section div').each(function () {
+      const hourEl = new Date();
+      hourEl.setHours(startHour);
+      // Change time display format
+      if (hourEl.getHours() < 12) {
+        this.append(hourEl.getHours() + "AM");
+      } else if (hourEl.getHours() > 12) {
+        this.append(hourEl.getHours() - 12 + "PM");
+      } else {
+        this.append(hourEl.getHours() + "PM");
+      }
+      startHour++;
+    });
+  }
 
   // Set startHour back to 9. Add class to rows
-  resetHour();
-  $('section').each(function () {
-    const hourEl = new Date();
-    hourEl.setHours(startHour);
-    var today = new Date();
+  function addClass() {
+    resetHour();
+    $('section').each(function () {
+      const hourEl = new Date();
+      hourEl.setHours(startHour);
+      var today = new Date();
+      // Compare display hour to currentHour and add class appropriately
+      if (hourEl < today) {
+        this.classList.add('past');
+      } else if (hourEl > today) {
+        this.classList.add('future');
+      } else {
+        this.classList.add('present');
+      }
+      startHour++;
+    });
+  }
 
-    // Compare display hour to currentHour and add class appropriately
-    if (hourEl < today) {
-      this.classList.add('past');
-    } else if (hourEl > today) {
-      this.classList.add('future');
-    } else {
-      this.classList.add('present');
-    }
-    startHour++;
-  });
+  // Stringify updated taskarray and save to local storage
+  function storeStorage() {
+    localStorage.setItem("current-tasks", JSON.stringify(taskArray))
+  }
 
-  // Listener for the save button
-  function handleFormSubmit() {
-    // Clears currently stored taskarray from local storage
-    localStorage.clear();
-    // Goes through each description input element and adds to array even if text is blank. Array should always be number of hour row divs
+  // Goes through each description input element and adds to array even if text is blank. Array should always be number of hour row divs
+  function addToArray() {
     $('section').each(function () {
       var descriptionInput = this.children[1].value;
       taskArray.push(descriptionInput);
     });
-    // Stringify updated taskarray and save to local storage
-    localStorage.setItem("current-tasks", JSON.stringify(taskArray))
   }
+
+  // Clears currently stored taskarray from local storage
+  function clearStorage() {
+    localStorage.clear();
+  }
+
+  // Clears current taskArray value
+  function clearArray() {
+    taskArray = [];
+  }
+
+  // Listener for the save button
+  function handleFormSubmit(event) {
+    event.preventDefault();
+    clearStorage();
+    clearArray();
+    addToArray();
+    storeStorage();
+  }
+
+  // Calls all functions to set up hour row divs
+  addTemplate();
+  renderArray();
+  setIds();
+  displayHours();
+  addClass();
+
   // Create a submit event listener on the form element
   container.on('submit', handleFormSubmit);
 });
+
